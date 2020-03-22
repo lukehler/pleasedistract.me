7ddf43725d03bd287e0f74ab7e51010ba00a912c
Luke Ehler <luke@sametz.com>
2020-03-22 17:25:20 -0400

    Fully functional MVP. Just add data.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,23 @@
-// TODO:
+// Variables
 
-// fetch the list of all links
-// function: get random link
-// function: format link
+var recommendations = document.getElementById('recommendations');
+var refresh = document.getElementById('refresh');
+var link = document.getElementById('link');
+
+// Function: get random link
+var updateLink = function(){
+	var length = links.length;
+	var index = Math.floor( Math.random() * length );
+	var linkObject = links[ index ];
+	var text = "Here's something " + linkObject.adjective + " from " + linkObject.author + ".";
+	link.innerText = text;
+	link.href = linkObject.link;
+}
+
+// And fire everything off.
+	// Initial link generation
+	updateLink();
+	// Bind event to button
+	refresh.addEventListener( 'click', updateLink );
+	// Bind event to link
+	link.addEventListener( 'click', updateLink );
